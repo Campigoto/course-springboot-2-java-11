e9c6d6e6726ff51f6b7c6b9b5f4e724cdec4713d
Campigoto <campigoto@hotmail.com>
2020-04-08 23:26:25 -0300

    H2 database, test profile, JPA

--- src/main/java/com/campigoto/course/entities/User.java
+++ src/main/java/com/campigoto/course/entities/User.java
@@ -2,9 +2,17 @@ package com.campigoto.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -14,6 +22,7 @@ public class User implements Serializable {
 	public User() {
 	}
 
+	
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
